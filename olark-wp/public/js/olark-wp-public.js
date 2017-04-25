b92e07749aaf5a9f306b94cff87d1357c0376ce0
Rhoda <rhoda@olark.com>
2017-04-25 21:34:35 +0100

    added console message if no additional api calls are made

--- olark-wp/public/js/olark-wp-public.js
+++ olark-wp/public/js/olark-wp-public.js
@@ -9,6 +9,7 @@ y.identify=function(i){y("identify",k.i=i)};
 y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
 k=y._={s:[],t:[+new Date],c:{},l:a};
 })(window,document,"static.olark.com/jsclient/loader.js");
+/* Add configuration calls below this comment */
 if (olark_vars.expand != 0){
 	olark.configure('box.start_expanded', true);
 	console.log("[olark] Wordpress start_expanded option is enabled");
@@ -25,7 +26,10 @@ if (olark_vars.float != 0){
 };
 olark.configure("system.localization", olark_vars.lang);
 console.log("[olark] Language chosen in Wordpress is " +olark_vars.lang);
-/* Add configuration calls bellow this comment */
 eval(olark_vars.api);
-console.log("[olark] additional api calls in Wordpress are as follows " +olark_vars.api);
+if (olark_vars.api.length < 2) {
+	console.log("[olark] There are no additional api calls used in the Wordpress plugin");
+}else {
+	console.log("[olark] additional api calls in Wordpress are as follows " +olark_vars.api);
+};
 olark.identify(olark_vars.site_ID);
